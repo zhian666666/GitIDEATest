6b5ed3c21e0a0127218cb1b9ff8b41ac8fba7ce9
zhian66666 <852964632@qq.com>
2022-06-20 22:11:35 +0800

    Initial commit

--- Moudle01/src/com/project/Test01.java
+++ Moudle01/src/com/project/Test01.java
@@ -3,8 +3,10 @@ package com.project;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("hello idea！");
+        //输出一个4+5=几的等式
+        int a = 4;
+        int b = 5;
+        System.out.println(a + "+" + b + "=" + (a + b));
 
-        int a = 0;
-        System.out.println(a);
     }
 }
